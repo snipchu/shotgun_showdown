5542001f866647828ab81b34a4eb204dea18ef19
bunnyguy <pipandw@gmail.com>
2024-11-26 12:20:32 -0600

    birdy script

--- scripts/birdy.gd
+++ scripts/birdy.gd
@@ -1,17 +1,15 @@
 extends AnimatableBody3D
 
-var direction : float
+var coin : float
 var speed : float
 
+func get_coin(): return coin
+
 func _ready() -> void:
-	direction = randf()
-	speed = randi_range(5, 10)
-	position.x += speed
+	coin = -1 if (randi_range(0,1)==0) else 1
+	speed = randf_range(2,5)
+	print(coin)
 
 func _process(delta: float) -> void:
-	if (direction>0.5):
-		pass
-	else:
-		pass
-	if (position.x < -12 || position.x > 12):
-		queue_free()
+	position.x += speed*delta if get_coin()==1  else -speed*delta
+	if (position.x>15 || position.x<-15): queue_free()
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,19 @@
 extends Node3D
-@export var birdyscene : PackedScene
+var birdyscene = preload("res://scenes/birdy.tscn")
+
+func _ready() -> void:
+	randomize()
+	$bird_spawn.start()
+	$gamelength.start()
 
 func _on_bird_spawn_timeout() -> void:
-	print("Timer ended!")
+	var birdy = birdyscene.instantiate()
+	birdy.position.x = 0
+	birdy.position.z = -10+(randf()*6)
+	birdy.position.y = randf_range(1,3)
+	print("Spawning bird at X="+str(birdy.position.x)+"\tY="+str(birdy.position.y)+"\tZ="+str(birdy.position.z))
+	add_child(birdy)
+
+func _on_gamelength_timeout() -> void:
+	print("Game has ended!")
+	$bird_spawn.stop()
